0121354470ad8f7746eb596d49646e8e02a188e2
chechaluque <cesarluque2000@gmail.com>
2016-09-15 22:06:17 -0700

    Login to the blog

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -17,11 +17,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^comments/', include('comments.urls', namespace='comments')),
     url(r'^posts/', include('posts.urls', namespace='posts')),
+    url(r'^login/', login_view, name='login'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
